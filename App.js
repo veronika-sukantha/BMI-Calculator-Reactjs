d8f89caa75180c4a5d79f269d9182b3dc934504e
veronika sukantha <85723282+veronika-sukantha@users.noreply.github.com>
2023-07-30 20:41:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import BmiCalculator from "./Components/BmiCalculator";
+function App() {
+    return (
+      <div className="App">
+     <BmiCalculator/>
+   </div>
+    );
+  }
+  
+  export default App;
